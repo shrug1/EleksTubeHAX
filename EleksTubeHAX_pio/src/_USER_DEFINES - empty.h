76ba4326abe13fbf6ed92f75ab8dece64833bf91
Clemens Sutor <clemenssutor@gmail.com>
2024-06-11 13:55:56 +0200

    more mods

--- EleksTubeHAX_pio/src/_USER_DEFINES - empty.h
+++ EleksTubeHAX_pio/src/_USER_DEFINES - empty.h
@@ -10,15 +10,19 @@
 #define USER_DEFINES_H_
 
 
-//#define DEBUG_OUTPUT
+//uncomment for Debug logs via serial interfaces
+#define DEBUG_OUTPUT 1
+#define DEBUG_OUTPUT_TFT 1
+//#define DEBUG_OUTPUT_MQTT 1
 
 
 // ************* Type of the clock hardware  *************
-#define HARDWARE_Elekstube_CLOCK  // uncomment for the original Elekstube clock
+//#define HARDWARE_Elekstube_CLOCK  // uncomment for the original Elekstube clock
 //#define HARDWARE_Elekstube_CLOCK_Gen2  // uncomment for the original Elekstube clock Gen2.1 (ESP32 Pico D4 Chip)
 //#define HARDWARE_SI_HAI_CLOCK  // uncomment for the SI HAI copy of the clock
-//#define HARDWARE_NovelLife_SE_CLOCK  // uncomment for the NovelLife SE version; non-SE not tested
+//#define HARDWARE_NovelLife_SE_CLOCK  // uncomment for the NovelLife SE version (Gesture only) - tested and working!; Non-SE version (Buttons only) NOT tested!; Pro version (Buttons and Gesture) NOT tested!
 //#define HARDWARE_PunkCyber_CLOCK  // uncomment for the PunkCyber / RGB Glow tube / PCBway clock
+#define HARDWARE_IPSTUBE_H401_CLOCK  // uncomment for the IPSTUBE Model H401 Clock
 
 
 // ************* Clock font file type selection (.clk or .bmp)  *************
@@ -39,6 +43,12 @@
 #define WIFI_SSID      "__enter_your_wifi_ssid_here__"       // not needed if WPS is used
 #define WIFI_PASSWD    "__enter_your_wifi_password_here__"   // not needed if WPS is used.  Caution - Hard coded password is stored as clear text in BIN file
 
+//  *************  Custom Timezone Offset *************
+// ONLY define this on, if no geolocation is wanted/avaiable and GEOLOCATION_ENABLED is NOT defined
+// you can use "+" and "-" in front of the value and decimal values. E.g.: "2" or "+2" or "-7" or "2.5" or "-4.75"
+// No check is done, so values over 24 will turn over, but work, strings will fail (leading to 0 as offset)
+// No auto-switch for Daylight-Saving-Time (Summertime) to Normal time with this! This is only avaiable with internet-based GeoLoc in the moment!
+#define CUSTOM_TIMEZONE_OFFSET "2"
 
 //  *************  Geolocation  *************
 // Get your API Key on https://www.abstractapi.com/ (login) --> https://app.abstractapi.com/api/ip-geolocation/tester (key) *************
