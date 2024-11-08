1feea7eea7f7126eb3784d7c2330623f29759112
Clemens Sutor <clemenssutor@gmail.com>
2024-11-08 20:37:27 +0100

    Prep for deletion

--- pre-built-firmware/_ESP32 save flash 8MB.cmd
+++
@@ -1,2 +0,0 @@
-esptool --port COM12 --baud 921600 read_flash 0x000000 0x800000 backup1.bin
-pause
--- pre-built-firmware/_ESP32 write flash.cmd
+++
@@ -1,2 +0,0 @@
-esptool --port COM56 --baud 921600 write_flash --erase-all 0x0000 backup1.bin
-pause
